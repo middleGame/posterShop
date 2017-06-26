12eba4b5a9662340eb011855b841c520e7054b86
Jaroslav Voevodin <itxjara@gmail.com>
2017-06-26 10:22:08 +0300

    final changes

--- public/script.js
+++ public/script.js
@@ -1,26 +1,48 @@
-var PRICE = 9.99
+var PRICE = 9.99;
+var LOAD_NUM = 10;
 
 new Vue({
 	el: '#app',
 	data: {
 		total: 0,
-		items: [
-			{
-				id: 1,
-				title: "Item 1"
-			},
-			{
-				id: 2,
-				title: "Item 2"
-			},
-			{
-				id: 3,
-				title: "Item 3"
-			}
-		],
-		cart: []
+		items: [],
+		cart: [],
+		results: [],
+		newSearch: 'fallout',
+		lastSearch: '',
+		loading: false,
+		price: PRICE
+	},
+	computed: {
+		noMoreItems: function(){
+			return this.items.length === this.results.length && this.results.length > 0;
+		}
 	},
 	methods: {
+		appendItems: function(){
+			// console.log('items: ' + this.items.length);
+			// console.log('results: ' + this.results.length);
+
+			if (this.items.length < this.results.length){
+				var append = this.results.slice(this.items.length, this.items.length + LOAD_NUM);
+				this.items = this.items.concat(append);
+			}
+		},
+		onSubmit: function() {
+			if (this.newSearch.length) {
+				this.items = [];
+				this.loading = true;
+				this.$http
+				  .get('/search/'.concat(this.newSearch))
+				  .then(function(res) {
+				  	this.lastSearch = this.newSearch;
+				  	this.results = res.data;
+					this.appendItems();
+					this.loading = false;
+			});
+			}
+			
+		},
 		addItem: function(index){
 			this.total += PRICE;
 			var item = this.items[index];
@@ -63,5 +85,16 @@ new Vue({
 		currency: function(price) {
 			return '$'.concat(price.toFixed(2));
 		}
+	},
+	mounted: function() {
+		this.onSubmit();
+
+		var vueInstance = this;
+		var elem = document.getElementById('product-list-bottom');
+		var watcher = scrollMonitor.create(elem);
+		watcher.enterViewport(function(){
+			vueInstance.appendItems();
+		});
 	}
 });
+
